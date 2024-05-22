b635c1a0c52ee273472eb197756b657f1125ead7
Subodh <subodh16728@gmail.com>
2024-05-22 14:24:37 +0530

    Code cleaning

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Playlists, Playlist, Profile, TopArtists, TopTracks } from "./pages";
-import Home from "./Home";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -17,10 +16,6 @@ export const router = createBrowserRouter([
   {
     path: "/playlists",
     element: <Playlists />,
-  },
-  {
-    path: "home",
-    element: <Home />,
   },
   {
     path: "/playlists/:id",
